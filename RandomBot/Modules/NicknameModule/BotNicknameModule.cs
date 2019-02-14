664ced0fb0fca35b2912462540235936fe9aa875
Adrian Christanto <adrianchristanto@gmail.com>
2019-02-14 17:47:27 +0700

    Add ChangeBotNickname Module

---
+++ RandomBot/Modules/NicknameModule/BotNicknameModule.cs
@@ -0,0 +1,28 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+
+namespace RandomBot.Modules.NicknameModule
+{
+    public class BotNicknameModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("botn", RunMode = RunMode.Async)]
+        public async Task ChangeBotNickName([Remainder]string newNickName)
+        {
+            await Context.Message.DeleteAsync();
+
+            if (Context.User.Id == 318035086375387136)
+            {
+                var user = Context.Guild.GetUser(371933819705753600);
+                await user.ModifyAsync(Q =>
+                {
+                    Q.Nickname = newNickName;
+                });
+            }
+            else
+            {
+                await Context.Channel.SendMessageAsync("D:");
+            }
+        }
+    }
+}
